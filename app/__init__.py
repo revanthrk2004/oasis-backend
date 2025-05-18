d7a3ccc52f4eccdb904c06797903418ce8c9b946
revanthrk2004 <revanthkkrishnan@gmail.com>
2025-05-18 06:04:17 +0100

    Initial commit

---
+++ app/__init__.py
@@ -0,0 +1,27 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+from flask_jwt_extended import JWTManager
+from .config import Config
+
+db = SQLAlchemy()
+jwt = JWTManager()
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+    jwt.init_app(app)
+    CORS(app)
+
+      # Register blueprints
+    from .routes import main
+    app.register_blueprint(main)
+
+    from . import models  # Register models
+
+    return app
