fd172ebd74b48375642b1a5b83c60ac5b9aae174
revanthrk2004 <revanthkkrishnan@gmail.com>
2025-06-16 00:50:52 +0100

    email

--- app/email_utils.py
+++ app/email_utils.py
@@ -1,16 +1,29 @@
 import sendgrid
-from sendgrid.helpers.mail import Mail
+from sendgrid.helpers.mail import Mail, Email, To, Content, Attachment, FileContent, FileName, FileType, Disposition
+import base64
 import os
 
-def send_email(to, subject, content):
+def send_email(to, subject, content, attachment=None, filename=None):
     try:
         sg = sendgrid.SendGridAPIClient(api_key=os.getenv("SENDGRID_API_KEY"))
         email = Mail(
-            from_email="oasisappreset@gmail.com",
-            to_emails=to,
+            from_email=Email("oasisappreset@gmail.com"),
+            to_emails=To(to),
             subject=subject,
-            plain_text_content=content
+            plain_text_content=Content("text/plain", content)
         )
+
+        # If attachment is provided
+        if attachment and filename:
+            encoded_file = base64.b64encode(attachment.read()).decode()
+            attached_file = Attachment(
+                FileContent(encoded_file),
+                FileName(filename),
+                FileType("image/png"),
+                Disposition("attachment")
+            )
+            email.attachment = attached_file
+
         response = sg.send(email)
         return response.status_code < 400
     except Exception as e:
